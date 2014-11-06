aa5c28d70d5dd8849cd4be2beb7b23103f5031ca
Esteve Fernandez <esteve@osrfoundation.org>
2014-11-06 14:44:30 -0800

    Added authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   SUPPRESS_NAVBAR_ON = %w(home)
 
+  before_filter :authenticate
+
   def show
     if params[:id] == 'home'
       @suppress_navbar = SUPPRESS_NAVBAR_ON.include?(params[:id])
@@ -8,5 +10,13 @@ class PagesController < ApplicationController
     end
 
     render template: "pages/#{params[:id]}"
+  end
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['AUTH_CLAHUB_USERNAME'] && password == ENV['AUTH_CLAHUB_PASSWORD']
+    end
   end
 end
